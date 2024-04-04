e158547aa60f194a74b3155fe93d889779be1cd9
Thomas Zemp <tom@dhis2.org>
2024-04-04 13:54:46 +0200

    chore: day2, task1 solution

--- solutions/src/navigation/Navigation.js
+++ solutions/src/navigation/Navigation.js
@@ -1,6 +1,6 @@
+import { Menu, MenuItem } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React from 'react'
-// @TODO: Import the `Menu` and `MenuItem` components
 import { useNavigate, useMatch } from 'react-router-dom'
 
 const NavigationItem = ({ path, label }) => {
@@ -9,14 +9,13 @@ const NavigationItem = ({ path, label }) => {
 
     // "null" when not active, "object" when active
     const routeMatch = useMatch(path)
+
     // path is matched if routeMatch is not null
-    //   eslint-disable-next-line no-unused-vars
     const isActive = Boolean(routeMatch)
 
     const onClick = () => navigate(path)
 
-    // @TODO: Use the `MenuItem` component insteaf of the `span`
-    return <div onClick={onClick}>{label}</div>
+    return <MenuItem label={label} active={isActive} onClick={onClick} />
 }
 
 NavigationItem.propTypes = {
@@ -25,8 +24,7 @@ NavigationItem.propTypes = {
 }
 
 export const Navigation = () => (
-    // @TODO: Use the `Menu` components instead of the `div`
-    <div>
+    <Menu>
         <NavigationItem
             // Menu item for the home page
             label="Home"
@@ -44,5 +42,5 @@ export const Navigation = () => (
             label="Form"
             path="/form"
         />
-    </div>
+    </Menu>
 )
