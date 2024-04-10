371283ca3b949202d2a69a714fc96abfa6d56b5b
Mozafar Haider <mozafar@dhis2.org>
2024-04-10 06:41:09 +0100

    fix(day3/task1.1): display API version from config

--- solutions/src/views/Home.js
+++ solutions/src/views/Home.js
@@ -1,10 +1,20 @@
+import { useConfig } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
+import { Tag } from '@dhis2/ui'
 import React from 'react'
 
-export const Home = () => (
-    <div>
-        <h1>{i18n.t('Home')}</h1>
+export const Home = () => {
+    const config = useConfig()
+    return (
+        <div>
+            <h1>{i18n.t('Home')}</h1>
 
-        <p>{i18n.t('DHIS2 Web App Academy 2024')}</p>
-    </div>
-)
+            <p>{i18n.t('DHIS2 Web App Academy 2024')}</p>
+            <Tag positive>
+                {i18n.t('Running on API version {{version}}', {
+                    version: config.apiVersion,
+                })}
+            </Tag>
+        </div>
+    )
+}
