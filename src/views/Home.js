e91e043d04625e3ce8dfb3529e79750cd14b519f
Thomas Zemp <tom@dhis2.org>
2024-04-16 09:11:33 +0000

    chore: make a change

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Home = () => (
     <div>
-        <h1>Home</h1>
+        <h1>Home (Tom's fork)</h1>
 
         <p>DHIS2 Web App Academy 2024</p>
     </div>
