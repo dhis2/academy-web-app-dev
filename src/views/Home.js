46d3a8127a8ebba02b996c3d434dbbbd8a2a2f27
Ynignandjo <67394037+Ynignandjo@users.noreply.github.com>
2024-04-16 10:00:16 +0000

    fix: add name

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 export const Home = () => (
     <div>
         <h1>Bienvenue, vous êtes sur DHIS2</h1>
+        <h2>Je suis YEO</h2>
 
         <p>{`DHIS2 Web App Academy 2024, Abidjan - Côte d'Ivoire`}</p>
     </div>
