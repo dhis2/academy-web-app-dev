a5e82b12a6f590048e24328cd0b76f240bcc3291
Mikee131 <michaeleshete9@gmail.com>
2024-04-16 11:45:43 +0000

    chore:Finished working on the second task of day2

--- src/views/Attributes.js
+++ src/views/Attributes.js
@@ -1,24 +1,37 @@
 import React from 'react'
 import { useGetAttributes } from '../hooks/index.js'
-
+import { Table ,DataTable, TableHead, TableCellHead, TableBody, TableRow, TableCell,} from '@dhis2/ui'
 export const Attributes = () => {
     // we get the data using a custom hook
     // we will update this implementation after learning about app-runtime
     const { loading, error, data } = useGetAttributes()
-
+    
     return (
         <div>
             <h1>Attributes</h1>
-            <p>loading: {JSON.stringify(loading)}</p>
-            <p>error message: {error?.message}</p>
-            {
-                // if there is any data available
-                data?.attributes?.attributes && (
-                    <pre>
-                        {JSON.stringify(data.attributes.attributes, null, 4)}
-                    </pre>
-                )
-            }
+            {/* <p>loading: {JSON.stringify(loading)}</p>
+            <p>error message: {error?.message}</p> */}
+            <Table>
+                <TableHead>
+                    <TableCellHead>Name</TableCellHead>
+                    <TableCellHead>Unique</TableCellHead>
+                </TableHead>
+
+                <TableBody>
+                    {data.attributes.attributes.map(
+                        ({id,displayName,unique})=>(
+                            <TableRow key={id}>
+                                <TableCell>{displayName}</TableCell>
+                                <TableCell>
+                                    {unique? 'Yes':'No'}
+                                </TableCell>
+                            </TableRow>
+                        )
+                    )}
+                </TableBody>
+            </Table>
+                 
+            
         </div>
     )
 }
