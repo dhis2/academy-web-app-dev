99ca646ed4d4d84692d96a6791cbfb4c8db0e5d7
Ynignandjo <67394037+Ynignandjo@users.noreply.github.com>
2024-04-16 09:52:01 +0000

    <mon premier commit>

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Home = () => (
     <div>
-        <h1>Home</h1>
+        <h1>Bienvenue, vous êtes sur DHIS2</h1>
 
-        <p>DHIS2 Web App Academy 2024</p>
+        <p>{`DHIS2 Web App Academy 2024, Abidjan - Côte d'Ivoire`}</p>
     </div>
 )
