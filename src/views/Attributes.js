2b013d0e22055b0e7aa616ca023e0d9012af48ee
mattae <matthew.edor@gmail.com>
2024-04-16 14:40:05 +0100

    - Task 2

--- src/views/Attributes.js
+++ src/views/Attributes.js
@@ -1,24 +1,52 @@
+import {
+    CenteredContent,
+    CircularLoader,
+    DataTable,
+    DataTableCell,
+    DataTableColumnHeader,
+    DataTableRow,
+    NoticeBox,
+    TableBody,
+    TableHead
+} from '@dhis2/ui'
 import React from 'react'
 import { useGetAttributes } from '../hooks/index.js'
 
 export const Attributes = () => {
     // we get the data using a custom hook
     // we will update this implementation after learning about app-runtime
-    const { loading, error, data } = useGetAttributes()
-
+    const {loading, error, data} = useGetAttributes()
+    if (loading) {
+        return <CenteredContent>
+            <CircularLoader/>
+        </CenteredContent>
+    }
+    if (error) {
+        return <NoticeBox error title="Error loading data">{error}</NoticeBox>
+    }
     return (
         <div>
             <h1>Attributes</h1>
-            <p>loading: {JSON.stringify(loading)}</p>
-            <p>error message: {error?.message}</p>
-            {
-                // if there is any data available
-                data?.attributes?.attributes && (
-                    <pre>
-                        {JSON.stringify(data.attributes.attributes, null, 4)}
-                    </pre>
-                )
-            }
+            {data?.attributes?.attributes && (
+                <DataTable>
+                    <TableHead>
+                        <DataTableColumnHeader>
+                            Name
+                        </DataTableColumnHeader>
+                        <DataTableColumnHeader>
+                            Unique
+                        </DataTableColumnHeader>
+                    </TableHead>
+                    <TableBody>
+                        {data.attributes.attributes.map((attr) => {
+                            return <DataTableRow key={attr.id}>
+                                <DataTableCell>{attr.displayName}</DataTableCell>
+                                <DataTableCell>{attr.unique ? 'Yes' : 'No'}</DataTableCell>
+                            </DataTableRow>
+                        })}
+                    </TableBody>
+                </DataTable>
+            )}
         </div>
     )
 }
