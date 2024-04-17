f24ce6d1f1d08b9d55e665a98661eb857d1e7470
mattae <matthew.edor@gmail.com>
2024-04-17 11:05:41 +0100

    Implement Runtime useConfig

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,16 @@
+import { useConfig } from '@dhis2/app-runtime';
 import i18n from '@dhis2/d2-i18n'
 import React from 'react'
 
-export const Home = () => (
-    <div>
-        <h1>{i18n.t('Home')}</h1>
+export const Home = () => {
+    const { apiVersion } = useConfig();
+    return (
+        <div>
+            <h1>{i18n.t('Home')}</h1>
 
-        <p>DHIS2 Web App Academy 2024</p>
-    </div>
-)
+            <p>DHIS2 Web App Academy 2024</p>
+
+            <p >{i18n.t('API_VERSION', {apiVersion})}</p>
+        </div>
+    )
+}
