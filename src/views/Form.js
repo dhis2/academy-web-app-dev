47b7449f61d3385c0f1c1daf5008f6719f1207cf
mattae <matthew.edor@gmail.com>
2024-04-17 10:29:04 +0100

    Implement form

--- src/views/Form.js
+++ src/views/Form.js
@@ -1,6 +1,18 @@
-import { ReactFinalForm } from '@dhis2/ui'
+import {
+    createEqualTo,
+    composeValidators,
+    email,
+    Button,
+    InputFieldFF,
+    ReactFinalForm,
+    SingleSelectFieldFF,
+    SwitchFieldFF,
+    hasValue,
+    dhis2Username
+} from '@dhis2/ui'
 import React from 'react'
 import styles from './Form.module.css'
+
 
 /**
  * This is just a function to demonstrate the values when the form is submitted
@@ -20,23 +32,121 @@ const alertValues = (values) => {
     alert(formattedValuesString)
 }
 
-const { Field, Form: RFForm } = ReactFinalForm
+const {Field, Form: RFForm} = ReactFinalForm
 
 export const Form = () => (
     <div>
         <h1>Form</h1>
 
         <RFForm onSubmit={alertValues}>
-            {({ handleSubmit }) => (
+            {({handleSubmit}) => (
                 <form onSubmit={handleSubmit}>
                     <div className={styles.row}>
-                        <Field
-                            name="surname"
-                            label="Surname"
-                            component={'input'}
-                            className={styles.surname}
-                            initialValue={'Traore'}
+                        <ReactFinalForm.Field
+                            name="title"
+                            label="Title"
+                            component={SingleSelectFieldFF}
+                            className={styles.title}
+                            initialValue="None"
+                            options={[
+                                {label: 'Professor', value: 'Professor'},
+                                {label: 'Doctor', value: 'Doctor'},
+                                {label: 'None', value: 'None'},
+                            ]}
                         />
+                    </div>
+                    <div className={`${styles.row} ${styles.full}`}>
+                        <div className={`${styles.half} ${styles.full}`}>
+                            <Field
+                                name="surname"
+                                label="Surname"
+                                required
+                                component={InputFieldFF}
+                                type="text"
+                                placeholder="Surname"
+                                className={`${styles.title} ${styles.full}`}
+                                validate={hasValue}
+                            />
+                        </div>
+                        <div className={`${styles.half} ${styles.full}`}>
+                            <Field
+                                name="firstname"
+                                label="First name"
+                                required
+                                component={InputFieldFF}
+                                type="text"
+                                placeholder="First name"
+                                className={`${styles.title} ${styles.full}`}
+                                validate={hasValue}
+                            />
+                        </div>
+                    </div>
+                    <div className={`${styles.row} ${styles.full}`}>
+                        <Field
+                            name="username"
+                            label="Username"
+                            required
+                            component={InputFieldFF}
+                            type="text"
+                            placeholder="Username"
+                            className={`${styles.title} ${styles.full}`}
+                            validate={composeValidators(hasValue, dhis2Username)}
+                        />
+                    </div>
+                    <div className={`${styles.row} ${styles.full}`}>
+                        <Field
+                            name="password"
+                            label="Password"
+                            required
+                            component={InputFieldFF}
+                            type="password"
+                            placeholder="Password"
+                            className={`${styles.title} ${styles.full}`}
+                            validate={hasValue}
+                        />
+                    </div>
+                    <div className={`${styles.row} ${styles.full}`}>
+                        <Field
+                            name="email"
+                            label="E-mail address"
+                            required
+                            component={InputFieldFF}
+                            type="email"
+                            placeholder="E-mail address"
+                            className={`${styles.title} ${styles.full}`}
+                            validate={composeValidators(email, hasValue)}
+                        />
+                    </div>
+                    <div className={`${styles.row} ${styles.full}`}>
+                        <Field
+                            name="confirm_email"
+                            label="Confirm E-mail address"
+                            required
+                            component={InputFieldFF}
+                            type="email"
+                            placeholder="Confirm e-mail address"
+                            className={`${styles.title} ${styles.full}`}
+                            validate={composeValidators(createEqualTo('email', hasValue))}
+                        />
+                    </div>
+                    <Field
+                        name="newsletter"
+                        label="I want to receive the newsletter"
+                        type="checkbox"
+                        component={SwitchFieldFF}
+                        className={`${styles.title} ${styles.full}`}
+                    />
+                    <div className={styles.row}>
+                        <Button
+                            ariaLabel="Button"
+                            name="Primary button"
+                            type="submit"
+                            primary
+                            title="Button"
+                            value="default"
+                        >
+                            Submit form
+                        </Button>
                     </div>
                 </form>
             )}
