96ede2553ac0e7a6eaa6cb35704f1a8bfcb6721a
mattae <matthew.edor@gmail.com>
2024-04-17 10:48:39 +0100

    Implement i18n

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,9 @@
+import i18n from '@dhis2/d2-i18n'
 import React from 'react'
 
 export const Home = () => (
     <div>
-        <h1>Home</h1>
+        <h1>{i18n.t('Home')}</h1>
 
         <p>DHIS2 Web App Academy 2024</p>
     </div>
