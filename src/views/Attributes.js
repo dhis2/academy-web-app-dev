09e9d4524ee2c51c7a5f0270c21987aed90714c7
mattae <matthew.edor@gmail.com>
2024-04-17 14:54:04 +0100

    Implement Runtime useDataQuery

--- src/views/Attributes.js
+++ src/views/Attributes.js
@@ -1,3 +1,5 @@
+import { useDataQuery } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
 import {
     CenteredContent,
     CircularLoader,
@@ -10,12 +12,25 @@ import {
     TableHead
 } from '@dhis2/ui'
 import React from 'react'
-import { useGetAttributes } from '../hooks/index.js'
+
+const PAGE_SIZE = 5
+const query = {
+    attributes: {
+        resource: 'attributes',
+        params: {
+            order: 'displayName:desc',
+            pageSize: PAGE_SIZE,
+        },
+    },
+    users: {
+        resource: 'me'
+    }
+}
 
 export const Attributes = () => {
     // we get the data using a custom hook
     // we will update this implementation after learning about app-runtime
-    const {loading, error, data} = useGetAttributes()
+    const { loading, error, data } = useDataQuery(query)
     if (loading) {
         return <CenteredContent>
             <CircularLoader/>
@@ -24,9 +39,14 @@ export const Attributes = () => {
     if (error) {
         return <NoticeBox error title="Error loading data">{error}</NoticeBox>
     }
+    console.log('Users', data?.users)
+    const name = data?.users?.name;
+    const email = data?.users?.email;
+
     return (
         <div>
             <h1>Attributes</h1>
+            <p>{i18n.t('ATTR_INFO', {name, email})}</p>
             {data?.attributes?.attributes && (
                 <DataTable>
                     <TableHead>
