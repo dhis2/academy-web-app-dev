3d3143155f8d477d87de1ed76016f9e3b431a176
Thomas Zemp <tom@dhis2.org>
2024-05-01 08:44:04 +0200

    chore: task 6

--- src/hooks/useUpdateSharing.js
+++ src/hooks/useUpdateSharing.js
@@ -1,17 +1,67 @@
-/* eslint no-unused-vars: 0 */ // @TODO remove this line after implementing
 import { useDataEngine } from '@dhis2/app-runtime'
 import { useCallback, useState } from 'react'
+import { DATASTORE_NAME } from '../constants.js'
 
-export const useUpdateSharing = () => {
+const METADATA_QUERY = {
+    metadata: {
+        resource: `dataStore/${DATASTORE_NAME}`,
+        id: ({ key }) => `${key}/metaData`,
+    },
+}
+
+const UPDATE_SHARING = {
+    resource: 'sharing',
+    params: ({ id }) => ({
+        type: 'dataStore',
+        id: id,
+    }),
+    type: 'update',
+    data: {
+        object: {
+            publicAccess: 'r-------',
+            externalAccess: false,
+            user: {},
+            userGroupAccesses: [],
+        },
+    },
+}
+
+export const useUpdateSharing = ({ sharingAlert }) => {
     const engine = useDataEngine()
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
     // you should define a sharing mutation that is passed a key parameter
-    const sharingMutation = useCallback(({ key }) => {
-        // first you need to look up the id for the datastore key
-        // then you need to post to the api to update the sharing
-    }, [])
+    const sharingMutation = useCallback(
+        ({ key }) => {
+            const updateSharing = async () => {
+                setLoading(true)
+                try {
+                    // first you need to look up the id for the datastore key
+                    const { metadata } = await engine.query(METADATA_QUERY, {
+                        variables: { key },
+                    })
+
+                    // then you need to post to the api to update the sharing
+                    const result = await engine.mutate(UPDATE_SHARING, {
+                        variables: { id: metadata?.id },
+                    })
+                    if (result?.status === 'OK') {
+                        sharingAlert({ success: true })
+                    }
+                } catch (e) {
+                    console.error(e)
+                    setError(e)
+                    sharingAlert({ success: false })
+                } finally {
+                    setLoading(false)
+                }
+            }
+
+            updateSharing()
+        },
+        [engine, sharingAlert]
+    )
 
     return [sharingMutation, { loading, error }]
 }
